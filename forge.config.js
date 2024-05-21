55d775fe6a7ff7defb98a3a8b9906a629edb4368
Andrew <andrew@nordicgamelab.org>
2024-05-20 20:05:34 -0400

    added electron

---
+++ forge.config.js
@@ -0,0 +1,59 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { utils: { fromBuildIdentifier } } = require('@electron-forge/core');
+
+module.exports = {
+  buildIdentifier: process.env.IS_BETA ? 'beta' : 'prod',
+  packagerConfig: {
+    asar: true,
+    appBundleId: fromBuildIdentifier({ beta: 'org.nordicgamelab.beta.learnHub', prod: 'org.nordicgamelab.learnHub' })
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'nordic-game-lab',
+          name: 'learnhub-desktop'
+        },
+        prerelease: false
+      }
+    }
+  ]
+};
